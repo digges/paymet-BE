f2b23fc3d8dd83550abc09fdda04aff7a6be2573
DIGGESH DASH01 <154720202+digges@users.noreply.github.com>
2025-12-25 18:42:27 +0530

    Update Controller.java

--- src/main/java/com/genie/Payment_Gateway/Controller/Controller.java
+++ src/main/java/com/genie/Payment_Gateway/Controller/Controller.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/payment")
-@CrossOrigin(origins = {"http://127.0.0.1:550","https://paytem-by.vercel.app","https://payment-fe-chi.vercel.app"})
+@CrossOrigin(origins = {"http://127.0.0.1:5550","https://paytem-by.vercel.app","https://payment-fe-chi.vercel.app,"})
 public class Controller {
     @Autowired
     private paymentService paymentService;
@@ -42,4 +42,5 @@ public class Controller {
 
     }
 }
+
 
