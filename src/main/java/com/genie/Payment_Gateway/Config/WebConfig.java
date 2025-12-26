730c0c1381bf636ea38b1fe5258dd2421b62b068
DIGGESH DASH01 <154720202+digges@users.noreply.github.com>
2025-12-26 20:47:14 +0530

    Update WebConfig.java

--- src/main/java/com/genie/Payment_Gateway/Config/WebConfig.java
+++ src/main/java/com/genie/Payment_Gateway/Config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5500","http://127.0.0.1:5500")
+                        .allowedOrigins("http://localhost:5500","http://127.0.0.1:5500","https://ecomm-fe-blond.vercel.app")
                         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS","PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
@@ -24,3 +24,4 @@ public class WebConfig {
         };
     }
 }
+
